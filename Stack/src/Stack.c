9901afa3bc7fa7ee50be3b0665f6af3e5a6f8fc2
titu1994 <titu1994@gmail.com>
2013-08-21 22:13:42 +0530

    Start

---
+++ Stack/src/Stack.c
@@ -0,0 +1,29 @@
+/*
+ ============================================================================
+ Name        : Stack.c
+ Author      : Somshubra Majumdar
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 50
+
+typedef struct Stact{
+	int a[SIZE];
+	int top;
+};
+
+int main(void) {
+
+
+
+
+	return EXIT_SUCCESS;
+}
+
+
